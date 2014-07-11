4f25df64bde4da08199801ab61441ff18b3c8524
arvindr21 <arvind.ravulavaru@gmail.com>
2014-07-11 08:25:42 +0530

    bump - 0.4.0

--- templates/app/scripts/main.js
+++ templates/app/scripts/main.js
@@ -1,4 +1,4 @@
-/**
+/*!
  *
  *  Web Starter Kit
  *  Copyright 2014 Google Inc. All rights reserved.
@@ -22,16 +22,19 @@
   var querySelector = document.querySelector.bind(document);
 
   var navdrawerContainer = querySelector('.navdrawer-container');
+  var body = document.body;
   var appbarElement = querySelector('.app-bar');
   var menuBtn = querySelector('.menu');
   var main = querySelector('main');
 
   function closeMenu() {
+    body.classList.remove('open');
     appbarElement.classList.remove('open');
     navdrawerContainer.classList.remove('open');
   }
 
   function toggleMenu() {
+    body.classList.toggle('open');
     appbarElement.classList.toggle('open');
     navdrawerContainer.classList.toggle('open');
   }
